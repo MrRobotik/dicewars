d2e8a4ee5775f491e5e4bfc744e626898554f10f
Adam Kucera <adamkucera3697@gmail.com>
2020-12-26 01:46:26 +0100

    train HAPP model

--- dicewars/ai/xkucer95/training.py
+++ dicewars/ai/xkucer95/training.py
@@ -10,22 +10,26 @@ from dicewars.ai.xkucer95.utils import batch_provider, evaluate
 
 
 def main():
+    epochs = 0
+    lr = 0.
     x_trn = None
-    x_eval = None
+    x_val = None
     t_trn = None
-    t_eval = None
+    t_val = None
     try:
         data_dir = sys.argv[1]
         model_name = sys.argv[2]
-        x_trn = np.loadtxt('{}/{}_x_trn.csv'.format(data_dir, model_name), delimiter=',').astype(np.float32)
-        x_eval = np.loadtxt('{}/{}_x_eval.csv'.format(data_dir, model_name), delimiter=',').astype(np.float32)
-        t_trn = np.loadtxt('{}/{}_t_trn.csv'.format(data_dir, model_name), delimiter=',').astype(np.float32)
-        t_eval = np.loadtxt('{}/{}_t_eval.csv'.format(data_dir, model_name), delimiter=',').astype(np.float32)
+        epochs = int(sys.argv[3])
+        lr = float(sys.argv[4])
+        x_trn = np.loadtxt('{}/{}_x_trn.csv'.format(data_dir, model_name), delimiter=' ').astype(np.float32)
+        x_val = np.loadtxt('{}/{}_x_val.csv'.format(data_dir, model_name), delimiter=' ').astype(np.float32)
+        t_trn = np.loadtxt('{}/{}_t_trn.csv'.format(data_dir, model_name), delimiter=' ').astype(np.float32)
+        t_val = np.loadtxt('{}/{}_t_val.csv'.format(data_dir, model_name), delimiter=' ').astype(np.float32)
     except:
         exit(1)
 
     model = HoldAreaProbPredictor()
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     batch_size = 32
 
     best_accuracy = 0.
@@ -33,7 +37,7 @@ def main():
     losses = []
     accuracies = []
 
-    for epoch in range(100):
+    for epoch in range(epochs):
         loss_avg = 0.
         for x, t in batch_provider(x_trn, t_trn, batch_size):
             y = model(x)
@@ -42,10 +46,11 @@ def main():
             optimizer.step()
             loss_avg += float(loss) * (1/len(t))
         with torch.no_grad():
-            accuracy = evaluate(model, x_eval, t_eval)
+            accuracy = evaluate(model, x_val, t_val)
         losses.append(loss_avg)
         accuracies.append(accuracy)
         if accuracy > best_accuracy:
+            print(accuracy)
             best_accuracy = accuracy
             best_model = copy.deepcopy(model)
 
@@ -53,8 +58,10 @@ def main():
     print('best acc.: {}'.format(best_accuracy))
     plt.subplot(2, 1, 1)
     plt.plot(losses, label='loss')
+    plt.legend(loc='upper right')
     plt.subplot(2, 1, 2)
     plt.plot(accuracies, label='accuracy')
+    plt.legend(loc='lower right')
     plt.show()
 
 
